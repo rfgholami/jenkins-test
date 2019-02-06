ff39c98e2f4835265b77ef321e7427629c61651c
Aref <rf.gholami@yahoo.com>
2019-02-06 10:09:32 +0330

    --

---
+++ src/main/java/com/rfgholami/samples/jenkinstest/JenkinsTestApplication.java
@@ -0,0 +1,28 @@
+package com.rfgholami.samples.jenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestApplication.class, args);
+    }
+
+}
+
+@RestController
+@RequestMapping(value = "/")
+class HelloController {
+
+    @GetMapping(value = "hello")
+    public String sayHello() {
+        return "Hello";
+    }
+
+}
+
